715234548263dc1efd8d5c104a05d409cc325d16
George Hotz <geohot@gmail.com>
2019-04-26 00:28:02 -0700

    comment out ml, run with cd

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 docker build -t battlechess -f Dockerfile .   # 1>&2
 if [ ! -z $1 ]; then
-  docker run -i --rm battlechess bash -c "cd /tmp/ && git clone -q https://github.com/$1/battlechess.git && python3 /tmp/battlechess/engine.py"
+  docker run -i --rm battlechess bash -c "cd /tmp/ && git clone -q https://github.com/$1/battlechess.git && cd battlechess && python3 engine.py"
 fi
 
