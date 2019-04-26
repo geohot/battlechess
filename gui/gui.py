b9ce421017d9c3b98d4cfb25c6712b0bafa3edfc
George Hotz <geohot@gmail.com>
2019-04-25 20:16:57 -0700

    gotta be REST

--- gui/gui.py
+++ gui/gui.py
@@ -10,6 +10,10 @@ from engine import get_move
 from flask import Flask, Response, request
 board = chess.Board()
 
+def computer_move(board):
+  if not board.is_game_over():
+    board.push(get_move(board))
+
 app = Flask(__name__)
 @app.route("/")
 def hello():
@@ -17,33 +21,30 @@ def hello():
   return ret.replace('start', board.fen())
 
 # moves given as coordinates of piece moved
-@app.route("/move_coordinates")
+@app.route("/move_coordinates", methods=['POST'])
 def move_coordinates():
   if not board.is_game_over():
-    source = int(request.args.get('from', default=''))
-    target = int(request.args.get('to', default=''))
-    promotion = True if request.args.get('promotion', default='') == 'true' else False
+    source = int(request.form.get('from', default=''))
+    target = int(request.form.get('to', default=''))
+    promotion = True if request.form.get('promotion', default='') == 'true' else False
 
     move = board.san(chess.Move(source, target, promotion=chess.QUEEN if promotion else None))
     if move is not None and move != "":
       try:
         board.push_san(move)
-        board.push(get_move(board))
+        computer_move(board)
       except Exception:
         traceback.print_exc()
-    return board.fen()
+  return board.fen()
 
-  print("GAME IS OVER")
-  return "game over"
-
-@app.route("/newgame")
+@app.route("/newgame", methods=['POST'])
 def newgame():
   board.reset()
   return board.fen()
 
-@app.route("/computermove")
+@app.route("/computermove", methods=['POST'])
 def computermove():
-  board.push(get_move(board))
+  computer_move(board)
   return board.fen()
 
 if __name__ == "__main__":
